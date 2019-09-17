15e7ed54979ef2fb84017e2ba6e8d0c83374bf2c
emerlino <enrico.merlino@libero.it>
2019-09-17 20:48:44 +0200

    Created maven project (just an Hello World)

---
+++ src/main/java/merlino/salestaxes/App.java
@@ -0,0 +1,13 @@
+package merlino.salestaxes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
