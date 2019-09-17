777ec3d226ab50d352b63bf4d71e96363275c2bb
emerlino <enrico.merlino@libero.it>
2019-09-17 21:15:41 +0200

    Tried to test App by AppTest

--- src/test/java/merlino/salestaxes/AppTest.java
+++ src/test/java/merlino/salestaxes/AppTest.java
@@ -1,20 +1,21 @@
 package merlino.salestaxes;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldGreetWorld()
     {
-        assertTrue( true );
+        assertThat(new App().hello("World"), is("Hello World!"));
+    }
+
+    @Test
+    public void shouldGreetBob()
+    {
+        assertThat(new App().hello("Bob"), is("Hello Bob!"));
     }
 }
