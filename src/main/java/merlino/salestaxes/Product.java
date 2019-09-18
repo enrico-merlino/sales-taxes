d986ae516a21df14d903e5f19f5ad3ab232cdc72
emerlino <enrico.merlino@libero.it>
2019-09-18 08:03:11 +0200

    Refactor: Product ready to apply 0 to N Tax(es)

--- src/main/java/merlino/salestaxes/Product.java
+++ src/main/java/merlino/salestaxes/Product.java
@@ -1,21 +1,26 @@
 package merlino.salestaxes;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static java.math.BigDecimal.ZERO;
 
 public class Product
 {
     private String _sku;
-    private Tax _tax;
+    private Tax[] _taxes;
 
-    public Product(String sku, Tax tax)
+    public Product(String sku, Tax... taxes)
     {
         _sku = sku;
-        _tax = tax;
+        _taxes = taxes;
     }
 
     public BigDecimal taxFor(BigDecimal currentPrice)
     {
-        return _tax.taxFor(currentPrice);
+        return Arrays.stream(_taxes)
+                .map(tax -> tax.taxFor(currentPrice))
+                .reduce(ZERO, (a, b) -> a.add(b));
     }
 
     public String sku()
